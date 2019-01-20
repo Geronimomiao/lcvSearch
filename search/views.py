7c94be9feeff4fca344d497f13ce5c4a93930975
wsm <1030057982@qq.com>
2019-01-20 12:16:31 +0800

    搜索功能 完成

--- search/views.py
+++ search/views.py
@@ -7,6 +7,8 @@ from elasticsearch import Elasticsearch
 from datetime import datetime
 import redis
 
+# elasticsearch 比 elasticsearch_dsl 更加底层的一种方法
+# elasticsearch_dsl 是在其之上的各种封装 对原生方法 比较熟 可以只用 elasticsearch
 client = Elasticsearch(hosts=["127.0.0.1"])
 redis_cli = redis.StrictRedis()
 
@@ -32,72 +34,75 @@ class SearchSuggest(View):
 
 class SearchView(View):
     def get(self, request):
-        key_words = request.GET.get("q","")
-        s_type = request.GET.get("s_type", "article")
-
-        redis_cli.zincrby("search_keywords_set", key_words)
-
-        topn_search = redis_cli.zrevrangebyscore("search_keywords_set", "+inf", "-inf", start=0, num=5)
-        page = request.GET.get("p", "1")
+        key_words = request.GET.get('q', '')
+        page = request.GET.get('p', '1')
         try:
             page = int(page)
         except:
             page = 1
 
-        jobbole_count = redis_cli.get("jobbole_count")
         start_time = datetime.now()
+
         response = client.search(
-            index= "jobbole",
+            index='jobbole',
             body={
-                "query":{
-                    "multi_match":{
-                        "query":key_words,
-                        "fields":["tags", "title", "content"]
+                "query": {
+                    "multi_match": {
+                        "query": key_words,
+                        "fields": ["tags", "title", "content"]
                     }
                 },
-                "from":(page-1)*10,
-                "size":10,
+                "from": (page - 1)*10,
+                "size": 10,
+                # 对搜索的字段 进行高亮处理
                 "highlight": {
-                    "pre_tags": ['<span class="keyWord">'],
+                    "pre_tags": ['<span class="keyword">'],
                     "post_tags": ['</span>'],
                     "fields": {
                         "title": {},
-                        "content": {},
+                        "content": {}
                     }
                 }
             }
         )
 
         end_time = datetime.now()
-        last_seconds = (end_time-start_time).total_seconds()
+        last_seconds = (end_time - start_time).total_seconds()
+
+
         total_nums = response["hits"]["total"]
-        if (page%10) > 0:
-            page_nums = int(total_nums/10) +1
-        else:
-            page_nums = int(total_nums/10)
         hit_list = []
         for hit in response["hits"]["hits"]:
-            hit_dict = {}
-            if "title" in hit["highlight"]:
-                hit_dict["title"] = "".join(hit["highlight"]["title"])
+            hit_dist = {}
+            if "title" in hit['highlight']:
+                hit_dist['title'] = "".join(hit["highlight"]["title"])
             else:
-                hit_dict["title"] = hit["_source"]["title"]
-            if "content" in hit["highlight"]:
-                hit_dict["content"] = "".join(hit["highlight"]["content"])[:500]
+                hit_dist['title'] = hit['_source']['title']
+            # content 取前 500 字
+            if "content" in hit['highlight']:
+                hit_dist['content'] = "".join(hit["highlight"]["content"])[:500]
             else:
-                hit_dict["content"] = hit["_source"]["content"][:500]
+                hit_dist['content'] = hit['_source']['content'][:500]
 
-            hit_dict["create_date"] = hit["_source"]["create_date"]
-            hit_dict["url"] = hit["_source"]["url"]
-            hit_dict["score"] = hit["_score"]
+            hit_dist['url'] = hit['_source']['url']
+            hit_dist['score'] = hit['_score']
 
-            hit_list.append(hit_dict)
+            hit_list.append(hit_dist)
 
-        return render(request, "result.html", {"page":page,
-                                               "all_hits":hit_list,
-                                               "key_words":key_words,
-                                               "total_nums":total_nums,
-                                               "page_nums":page_nums,
-                                               "last_seconds":last_seconds,
-                                               "jobbole_count":jobbole_count,
-                                               "topn_search":topn_search})
+        if(page%10) > 0:
+            page_nums = int(total_nums/10) + 1
+        else:
+            page_nums = int(total_nums/10)
+
+
+        # 将 key_words 传回去 是为了让其显示在 搜索栏
+        return render(request, 'result.html', {
+            "all_hits": hit_list,
+            "key_words": key_words,
+            "page": page,
+            "total_nums": total_nums,
+            "page_nums": page_nums,
+            "last_seconds": last_seconds
+        })
+
+
